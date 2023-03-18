b7a8a2ffbb901d72d50a37e5660246a4724f3247
Miran-Mirantee <killerm11805@hotmail.com>
2023-03-19 01:41:53 +0700

    Implement capitalize feature

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,24 +1,28 @@
 const capitalize = (input) => {
   let string = String(input);
 
+  // clear space in front
   for (let char of string) {
     if (char.includes(" ")) {
       string = string.slice(1);
     } else break;
   }
 
+  // check if input exists
   if (!input || string.length === 0) {
     throw new Error("Input cannot be blank");
   }
 
+  // check if character is letter of space
   for (let char of string) {
-    let result = char.match(/[a-z]/i);
+    let result = char.match(/[a-z|\s]/i);
     if (!result) {
       throw new Error("Letters only");
     }
   }
 
-  return string;
+  return (string =
+    string.charAt(0).toUpperCase() + string.slice(1).toLowerCase());
 };
 
 export default capitalize;
--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -23,3 +23,19 @@ test("Input empty string", () => {
 test("Input space string", () => {
   expect(() => capitalize("     ")).toThrow("Input cannot be blank");
 });
+
+test("Input lowercase string", () => {
+  expect(capitalize("abc")).toMatch(/Abc/);
+});
+
+test("Input uppercase string", () => {
+  expect(capitalize("ABC")).toMatch(/Abc/);
+});
+
+test("Input string with space in front", () => {
+  expect(capitalize("      abc")).toMatch(/Abc/);
+});
+
+test("Input string with space in between", () => {
+  expect(capitalize("i CAnNot TyPE at ALL")).toMatch(/I cannot type at all/);
+});
