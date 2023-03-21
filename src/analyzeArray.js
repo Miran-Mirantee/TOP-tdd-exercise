4e2c5eba200d9bf47213e1aee3ec099e2a1d678e
Miran-Mirantee <killerm11805@hotmail.com>
2023-03-22 01:35:57 +0700

    Implement analyzeArray and its test

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,9 +1,28 @@
 const analyzeArray = (array) => {
+  if (array === undefined) throw new Error("Input is required");
+  if (!Array.isArray(array)) throw new Error("Please input an array");
+  for (let num of array) {
+    if (isNaN(num))
+      throw new Error("The content inside array must be number only");
+  }
+
+  let length = array.length;
+  let average =
+    array.reduce((accumulator, currentValue) => accumulator + currentValue, 0) /
+    length;
+  let min = 999;
+  let max = -999;
+
+  for (let num of array) {
+    if (num < min) min = num;
+    if (num > max) max = num;
+  }
+
   const object = {
-    average: 4,
-    min: 1,
-    max: 8,
-    length: 6,
+    average,
+    min,
+    max,
+    length,
   };
   return object;
 };
--- test/analyzeArray.test.js
+++ test/analyzeArray.test.js
@@ -1,10 +1,42 @@
 import analyzeArray from "../src/analyzeArray";
 
-test("test", () => {
-  expect(analyzeArray()).toMatchObject({
+test("Input nothing", () => {
+  expect(() => analyzeArray()).toThrow("Input is required");
+});
+
+test("Input non-array", () => {
+  expect(() => analyzeArray(123)).toThrow("Please input an array");
+});
+
+test("Input string array", () => {
+  expect(() => analyzeArray([1, "a", 3, "asasd"])).toThrow(
+    "The content inside array must be number only"
+  );
+});
+
+test("Input array (1)", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toMatchObject({
     average: 4,
     min: 1,
     max: 8,
     length: 6,
+  });
+});
+
+test("Input array (2)", () => {
+  expect(analyzeArray([6, 9, 3, 5])).toMatchObject({
+    average: 5.75,
+    min: 3,
+    max: 9,
+    length: 4,
+  });
+});
+
+test("Input array (3)", () => {
+  expect(analyzeArray([5, 6, 9, 1, 3, 9, 5, 0])).toMatchObject({
+    average: 4.75,
+    min: 0,
+    max: 9,
+    length: 8,
   });
 });
